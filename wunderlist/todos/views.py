7ad27c574569db16205955bd49422348c3bd0273
euijin <kup90@naver.com>
2019-09-11 12:23:24 +0900

    update crud

--- wunderlist/todos/views.py
+++ wunderlist/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 
 # Create your views here.
@@ -9,34 +9,34 @@ def index(request):
     }
     return render(request, 'todos/index.html', ctx)
 
-def new(request):
-    return render(request, 'todos/new.html')
-
 def create(request):
-    title = request.POST.get('title')
-    due_date = request.POST.get('due-date')
-    
-    Todo.objects.create(title=title, due_date =due_date)
-    return redirect('todos:index')
-
-def edit(request, pk):
-    todo = Todo.objects.get(id=pk)
-    ctx = {
-        'todo':todo,
-    }
-    return render(request, 'todos/edit.html', ctx)
+    # new와 create를 통합
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due-date')
+        
+        Todo.objects.create(title=title, due_date =due_date)
+        return redirect('todos:index')
+    else:
+        return render(request, 'todos/create.html')
+        
 
 def update(request, pk):
-    title = request.POST.get('title')
-    due_date = request.POST.get('due-date')
-
-    todo = Todo.objects.get(id=pk)
-    todo.title = title
-    todo.due_date = due_date
-    todo.save()
-    return redirect('todos:index')
+    todo = get_object_or_404(Todo, id=pk)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due-date')
+        todo.title = title
+        todo.due_date = due_date
+        todo.save()
+        return redirect('todos:index')
+    else:
+        ctx = {
+            'todo':todo,
+        }
+        return render(request, 'todos/update.html', ctx)
 
 def delete(reqeust, pk):
-    todo = Todo.objects.get(id=pk)
+    todo = get_object_or_404(Todo, id=pk)
     todo.delete()
     return redirect('todos:index')
